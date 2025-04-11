59db084320b2300ac6d4ff55463497eac05ca60c
Brenden Murphy <brendenmurphy@gmail.com>
2025-04-10 22:42:45 -0500

    Initial project commit.

---
+++ Reactivities/Persistence/Migrations/20250410052154_ChangePropertyName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangePropertyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitutde",
+                table: "Activities",
+                newName: "Longitude");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Longitude",
+                table: "Activities",
+                newName: "Longitutde");
+        }
+    }
+}
